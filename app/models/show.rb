efe26c2e965f631d996492228f918c2f4004530c
Jane Venter <janeanneventer@gmail.com>
2019-02-14 20:26:21 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,7 +3,17 @@ class Show < ActiveRecord::Base
     Show.maximum("rating")
   end
   
+  def most_popular_show 
+    Show.where("rating" = highest_rating)
+  end
+
+  def lowest_rating 
+    Show.minimum("rating")
+  end
   
+  def least_popular_show 
+    Show.where("rating" = lowest_rating)
+  end
   
   
 end
